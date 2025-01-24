af16fa5ce0644ffb19761e4d30fc9aef1a7cd934
Miqayel <miqayel@slfstrg.com>
2025-01-24 11:32:01 +0400

    changes

--- js/script.js
+++ js/script.js
@@ -1,42 +1,52 @@
-const floors = document.querySelectorAll(".floor div button");
 const elevators = document.querySelectorAll(".elevator div");
+const floor = document.querySelector(".floor");
+const floorCount = 20;
+
 const arr = [
   { elevator: 1, count: 0 },
   { elevator: 2, count: 0 },
   { elevator: 3, count: 0 },
 ];
-``;
-floors.forEach((elem, index) =>
-  elem.addEventListener("click", () => {
-    const currentFloor = (index - (floors.length - 1)) * -1 + 1;
 
-    const currentElevator = [];
+function createElem(teg, text, attribute) {
+  const newElem = document.createElement(teg);
+  newElem.textContent = text;
+  if (attribute) newElem.setAttribute("index", attribute);
+  return newElem;
+}
 
-    arr.forEach((elem) => {
-      let currentElement = elem.count - currentFloor;
-      if (currentElement < 0) {
-        currentElement = currentElement * -1;
-        currentElevator.push(currentElement);
-      } else {
-        currentElevator.push(currentElement);
-      }
-    });
+for (let i = floorCount; i >= 1; i--) {
+  const div = createElem("div", `floor ${i}`);
+  div.appendChild(createElem("button", "elevator", `${i}`));
+  floor.appendChild(div);
+}
 
-    const min = currentElevator.reduce(
-      (acc, elem, index) => {
-        if (acc.count > elem) acc = { ind: index, count: elem };
-        return acc;
-      },
-      { ind: 0, count: currentElevator[0] }
-    );
+floor.addEventListener("click", function (e) {
+  if (e.target.nodeName !== "BUTTON") return;
+  const currentFloor = e.target.getAttribute("index");
 
-    arr[min.ind].count = currentFloor;
+  const currentElevator = [];
 
-    elevators[min.ind].style.transform = `translateY(${
-      index - (floors.length - 1)
-    }00%)`;
-  })
-);
+  arr.forEach((elem) => {
+    let currentElement = elem.count - currentFloor;
+    if (currentElement < 0) currentElement = currentElement * -1;
+    currentElevator.push(currentElement);
+  });
+
+  const min = currentElevator.reduce(
+    (acc, elem, index) => {
+      if (acc.count > elem) acc = { ind: index, count: elem };
+      return acc;
+    },
+    { ind: 0, count: currentElevator[0] }
+  );
+
+  arr[min.ind].count = currentFloor;
+
+  console.log(currentFloor);
+
+  elevators[min.ind].style.transform = `translateY(-${currentFloor - 1}00%)`;
+});
 
 // -------------------------------------------------------------------
 
